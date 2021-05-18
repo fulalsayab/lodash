00255b3d5aad7294eb7d54db5b5612002d0135da
falsaya <falsaya@ncsu.edu>
2021-05-18 12:35:04 -0400

    better implementation for clamp function

--- _.js
+++ _.js
@@ -1,26 +1,11 @@
 // object represents our library containing all the functionality we add to it.
 const _ = {
   // Clamps number within the inclusive lower and upper bounds.
-    clamp(number, lowerBound, upperBound) {
-        // cheacking if the lowerBound > upperBound
-        if (lowerBound > upperBound) {
-            let temp;
-            temp = lowerBound;
-            lowerBound = upperBound;
-            upperBound = temp;        
-        }
-      // If number < lower bound, return the lower bound as the final number
-      if (number < lowerBound) {
-        number = lowerBound;
-        return number;
-      } else if (number > upperBound) { // If number > upper bound, return the upper bound as the final number
-          number = upperBound;
-          return number;
-      } else {
-          return number;
-      }
-    },
-    
+  clamp(number, lowerBound, upperBound) {
+        let value = Math.max(number, lowerBound);
+        let final = Math.min(value, upperBound);
+        return final;
+  },
 };
 
 
